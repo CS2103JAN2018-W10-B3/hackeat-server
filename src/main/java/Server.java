5d27d6bb950df594b0a7268d0cfef08222b5387b
Sam Xie <samxie@outlook.com.au>
2018-04-04 16:53:46 +0800

    Initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,30 @@
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+
+/**
+ * A service which stores TwiML XML data or every order ID.
+ */
+public class Server {
+
+    /**
+     * Stores new order data for TwiML to fetch (via POST method)
+     * @param args
+     */
+    public static void main(String[] args) {
+        port(4569);
+
+        HashMap<String, String> map = new HashMap<>();
+
+        // Stores order data in memory
+        post("/create/:orderId", (req, res) -> {
+            String id = req.params(":orderId");
+            map.put(id, req.body());
+            return "SUCCESS";
+        });
+
+        // Releases data to TwiML
+        post("/order/:orderId", (req, res) -> map.get(req.params(":orderId")));
+    }
+}
